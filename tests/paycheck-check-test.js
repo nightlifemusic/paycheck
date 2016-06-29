5b86dd6f18e5d45b233212d65709774a277e89e5
Rene Wooller <renewooller@Renes-MacBook-Pro.local>
2016-06-29 12:52:20 +1000

    finishes basic test for check

--- tests/paycheck-check-test.js
+++ tests/paycheck-check-test.js
@@ -1,21 +1,55 @@
-var paycheck = require('../paycheck.js')({})
+var paycheck = new (require('../paycheck.js'))({})
 var assert = require("assert");
-var expect = require("chai").expect;
+var chai = require("chai");
+var expect = chai.expect;
 var _ = require('lodash')
 var sinon = require("sinon");
 var sinonChai = require("sinon-chai");
+chai.should();
+chai.use(sinonChai);
 
 describe('paycheck', function () {
     describe('check', function () {
         this.timeout(60000);
 
         it('should be able to check JSON rpc payloads against templates', function (done) {
-            //TODO mock out match method and unit test the check method
+            var jsonrpc = {
+                jsonrpc : '2.0',
+                method : 'service.controller.function',
+                params : {"a" : "b", "c" : {"d" : {"e" : "f"}}},
+                id : 1
+            }
+            
+            var templates = {
+                service : { controller : { function : [
+                    {myTemplate: 
+                        {
+                            jsonrpc : '2.0', 
+                            method: 'service.controller.function', 
+                            id : "<%=*%>", 
+                            params: {"a" : "<%=*%>", "c" : {"d" : {"e" : ["f", "g"]}}}
+                        }
+                    },
+                    {myOtherTemplate: 
+                        {
+                            jsonrpc : '2.0', 
+                            method: 'service.controller.function', 
+                            id : "<%=*%>", 
+                            params: {"a" : ["b","z"], "c" : {"d" : {"e" : "f"}}}
+                        }
+                    }
+                ]}}
+            }
+
+            var checkSpy = sinon.spy(paycheck, "match");
+
+            paycheck.check(jsonrpc, templates)
+
+            checkSpy.should.have.been.calledOnce;
+            checkSpy.should.have.been.calledWith(jsonrpc, templates.service.controller.function[0].myTemplate)
+
             done();
 
         })
-
-        
-
     })
 })
